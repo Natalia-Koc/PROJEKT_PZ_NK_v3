da3cb48acfb1becab065d84e88dde3db0979c3f4
Natalia-Koc <koc.natalia249@gmail.com>
2021-08-26 20:12:47 +0200

    zgłoszenia pod ofertą

--- Controllers/OffersController.cs
+++ Controllers/OffersController.cs
@@ -75,6 +75,12 @@ namespace PROJEKT_PZ_NK_v3.Controllers
                 return HttpNotFound();
             }
             ViewBag.IsApplied = db.Applications.Any(app => app.Guardian.Email == User.Identity.Name && app.OfferID == id);
+            List<Applications> AppList = db.Applications
+                .Include(a => a.Offer)
+                .Include(a => a.Guardian)
+                .Where(app => app.Owner.Email == User.Identity.Name && app.OfferID == id)
+                .ToList();
+            ViewBag.Applications = AppList;
             return View(offer);
         }
 
