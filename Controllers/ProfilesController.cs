5a8a948bf6b2e94db69e0cfe4f6d0747a901864f
Natalia-Koc <koc.natalia249@gmail.com>
2021-09-15 10:35:10 +0200

    ostatnie zmiany na Neo4J, przed testowaniem

--- Controllers/ProfilesController.cs
+++ Controllers/ProfilesController.cs
@@ -18,29 +18,99 @@ namespace PROJEKT_PZ_NK_v3.Controllers
     public class ProfilesController : Controller
     {
         private OfferContext db = new OfferContext();
-
-        public ActionResult Ranking()
+        public async Task<ActionResult> Ranking()
         {
-            var profiles = db.Profiles.OrderByDescending(p => p.Rate).Take(20).ToList();
-            return View(profiles);
-        }
-        public async Task<ActionResult> Details()
-        {
-            Profile profile = db.Profiles.FirstOrDefault(p => p.Email == User.Identity.Name);
-            /*Comments comments = new Comments();
-            ViewBag.comments = comments;
-            ViewBag.ProgressBarCount = db.Comments.Where(m => m.Profile.Email == User.Identity.Name && m.Grade != 0).Count();
-            ViewBag.FoundComment = db.Comments.Any(m => m.Author.Email == User.Identity.Name);*/
-
+            List<Profile> profiles = new List<Profile>();
             IAsyncSession session = db._driver.AsyncSession();
             try
             {
+                await session.ReadTransactionAsync(async tx =>
+                {
+                    var cursor =
+                        await tx.RunAsync(
+                            "match (p:Profile) return p");
+
+                    List<IRecord> Records = await cursor.ToListAsync();
+                    foreach (var item in Records)
+                    {
+                        INode nodeProfile = (INode)item.Values["p"];
+
+                        Profile profile = new Profile
+                        {
+                            HouseNumber = nodeProfile.Properties.Values.First().As<string>(),
+                            Email = nodeProfile.Properties.Values.Skip(1).First().As<string>(),
+                            Rate = nodeProfile.Properties.Values.Skip(2).First().As<int>(),
+                            FirstName = nodeProfile.Properties.Values.Skip(3).First().As<string>(),
+                            Street = nodeProfile.Properties.Values.Skip(4).First().As<string>(),
+                            PhoneNumber = nodeProfile.Properties.Values.Skip(5).First().As<string>(),
+                            City = nodeProfile.Properties.Values.Skip(6).First().As<string>(),
+                            Login = nodeProfile.Properties.Values.Skip(7).First().As<string>(),
+                            LastName = nodeProfile.Properties.Values.Skip(8).First().As<string>()
+                        };
+                        profiles.Add(profile);
+                    }
+
+                });
+
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+            return View(profiles.OrderByDescending(p => p.Rate).Take(20).ToList());
+        }
+        public async Task<ActionResult> Details()
+        {
+            Profile myProfile = new Profile();
+            IAsyncSession session = db._driver.AsyncSession();
+            try
+            {
+                await session.ReadTransactionAsync(async tx =>
+                {
+                    var cursor =
+                        await tx.RunAsync(
+                            "match (p:Profile {Email: '"+ User.Identity.Name + "'})<-[rel:COMMENTED_PROFILE]-(c:Comment) " +
+                            "return p,c");
+
+                    IRecord Record = await cursor.SingleAsync();
+                    INode nodeProfile = (INode)Record.Values["p"];
+
+                    myProfile.HouseNumber = nodeProfile.Properties.Values.First().As<string>();
+                    myProfile.Email = nodeProfile.Properties.Values.Skip(1).First().As<string>();
+                    myProfile.Rate = nodeProfile.Properties.Values.Skip(2).First().As<int>();
+                    myProfile.FirstName = nodeProfile.Properties.Values.Skip(3).First().As<string>();
+                    myProfile.Street = nodeProfile.Properties.Values.Skip(4).First().As<string>();
+                    myProfile.PhoneNumber = nodeProfile.Properties.Values.Skip(5).First().As<string>();
+                    myProfile.City = nodeProfile.Properties.Values.Skip(6).First().As<string>();
+                    myProfile.Login = nodeProfile.Properties.Values.Skip(7).First().As<string>();
+                    myProfile.LastName = nodeProfile.Properties.Values.Skip(8).First().As<string>();
+
+                    List<IRecord> records = await cursor.ToListAsync();
+                    List<Comments> comments = new List<Comments>();
+                    foreach (var item in records)
+                    {
+                        INode nodeComment = (INode)item.Values["c"];
+                        Comments comm = new Comments
+                        {
+                            Contents = nodeComment.Properties.Values.First().As<string>(),
+                            Grade = nodeComment.Properties.Values.Skip(1).First().As<int>(),
+                            ProfilEmail = nodeProfile.Properties.Values.Skip(1).First().As<string>(),
+                            ProfilLogin = nodeProfile.Properties.Values.Skip(7).First().As<string>()
+                        };
+
+                        comments.Add(comm);
+                    }
+
+                    ViewBag.ProgressBarCount = comments.Where(m => m.Grade != 0).Count();
+                    ViewBag.FoundComment = true;
+                });
 
                 await session.ReadTransactionAsync(async tx =>
                 {
                     var cursor =
                         await tx.RunAsync(
-                            "match (p:Profile)-[rell:AUTHOR]->(c:Comment)-[rel:COMMENTED_PROFILE]->(p2:Profile {Email: '" + User.Identity.Name + "'}) return c, p");
+                            "match (p:Profile)-[rell:AUTHOR]->(c:Comment)-[rel:COMMENTED_PROFILE]->(p2:Profile {Email: '" + User.Identity.Name + "'}) " +
+                            "return c, p");
 
                     /*ViewBag.Profile = cursor.SingleAsync().Result.Values.First().Value.As<string>();*/
 
@@ -53,8 +123,8 @@ namespace PROJEKT_PZ_NK_v3.Controllers
                         Comments comm = new Comments();
                         comm.Contents = node.Properties.Values.First().As<string>();
                         comm.Grade = node.Properties.Values.Skip(1).First().As<int>();
-                        comm.AuthorEmail = nodeProfile.Properties.Values.Skip(1).First().As<string>();
-                        comm.AuthorLogin = nodeProfile.Properties.Values.Skip(7).First().As<string>();
+                        comm.ProfilEmail = nodeProfile.Properties.Values.Skip(1).First().As<string>();
+                        comm.ProfilLogin = nodeProfile.Properties.Values.Skip(7).First().As<string>();
 
                         lista.Add(comm);
                     }
@@ -68,57 +138,103 @@ namespace PROJEKT_PZ_NK_v3.Controllers
                 await session.CloseAsync();
             }
 
-            return View(profile);
+            return View(myProfile);
         }
 
-        public async Task<ActionResult> DetailsAnotherProfile(int? id)
+        public async Task<ActionResult> DetailsAnotherProfile(string email)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Profile profile = db.Profiles.Find(id); 
-            /*ViewBag.ProgressBarCount = db.Comments.Where(m => m.ProfileID == id && m.Grade != 0).Count();
-            ViewBag.FoundComment = db.Comments.Any(m => m.Author.Email == User.Identity.Name && m.ProfileID == id);
-            if (!ViewBag.FoundComment)
-            {
-                ViewBag.MyComment = null;
-            }
-            else
-            {
-                ViewBag.MyComment = db.Comments.First(m => m.Author.Email == User.Identity.Name);
-            }*/
 
+            Profile anotherProfile = new Profile();
             IAsyncSession session = db._driver.AsyncSession();
             try
             {
-
                 await session.ReadTransactionAsync(async tx =>
                 {
                     var cursor =
                         await tx.RunAsync(
-                            "match (p:Profile)-[rell:AUTHOR]->(c:Comment)" +
-                                "-[rel:COMMENTED_PROFILE]->" +
-                                "(p2:Profile {Email: '" + db.Profiles.Where(m => m.ID == id).First().Email + "'}) return c, p");
+                            "match (p:Profile {Email: '" + email + "'})<-[rel:COMMENTED_PROFILE]-(c:Comment)<-[a:AUTHOR]-(p2:Profile) " +
+                            "return p,c,p2");
 
-                    /*ViewBag.Profile = cursor.SingleAsync().Result.Values.First().Value.As<string>();*/
+                    IRecord Record = await cursor.SingleAsync();
+                    INode nodeProfile = (INode)Record.Values["p"];
 
-                    List<IRecord> Records = await cursor.ToListAsync();
-                    List<Comments> lista = new List<Comments>();
-                    foreach (var item in Records)
+                    anotherProfile.HouseNumber = nodeProfile.Properties.Values.First().As<string>();
+                    anotherProfile.Email = nodeProfile.Properties.Values.Skip(1).First().As<string>();
+                    anotherProfile.Rate = nodeProfile.Properties.Values.Skip(2).First().As<int>();
+                    anotherProfile.FirstName = nodeProfile.Properties.Values.Skip(3).First().As<string>();
+                    anotherProfile.Street = nodeProfile.Properties.Values.Skip(4).First().As<string>();
+                    anotherProfile.PhoneNumber = nodeProfile.Properties.Values.Skip(5).First().As<string>();
+                    anotherProfile.City = nodeProfile.Properties.Values.Skip(6).First().As<string>();
+                    anotherProfile.Login = nodeProfile.Properties.Values.Skip(7).First().As<string>();
+                    anotherProfile.LastName = nodeProfile.Properties.Values.Skip(8).First().As<string>();
+
+                    List<IRecord> records = await cursor.ToListAsync();
+                    List<Comments> comments = new List<Comments>();
+                    foreach (var item in records)
                     {
-                        INode node = (INode)item.Values["c"];
-                        INode nodeProfile = (INode)item.Values["p"];
-                        Comments comm = new Comments();
-                        comm.Contents = node.Properties.Values.First().As<string>();
-                        comm.Grade = node.Properties.Values.Skip(1).First().As<int>();
-                        comm.AuthorEmail = nodeProfile.Properties.Values.Skip(1).First().As<string>();
-                        comm.AuthorLogin = nodeProfile.Properties.Values.Skip(7).First().As<string>();
+                        INode nodeComment = (INode)item.Values["c"];
+                        INode nodeAuthor = (INode)item.Values["p2"];
+                        Comments comm = new Comments
+                        {
+                            Contents = nodeComment.Properties.Values.First().As<string>(),
+                            Grade = nodeComment.Properties.Values.Skip(1).First().As<int>(),
+                            ProfilEmail = nodeAuthor.Properties.Values.Skip(1).First().As<string>(),
+                            ProfilLogin = nodeAuthor.Properties.Values.Skip(7).First().As<string>()
+                        };
 
-                        lista.Add(comm);
+                        comments.Add(comm);
                     }
-                    ViewBag.Comments = lista;
+                    ViewBag.Comments = comments;
 
+                    ViewBag.ProgressBarCount = comments.Where(m => m.Grade != 0).Count();
+                    ViewBag.FoundComment = comments.Any();
+                    if (!ViewBag.FoundComment)
+                    {
+                        ViewBag.MyComment = null;
+                    }
+                    else
+                    {
+                        ViewBag.MyComment = comments.First(m => m.ProfilEmail == User.Identity.Name);
+                    }
+                });
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+
+            if (anotherProfile == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Details", anotherProfile);
+        }
+
+        // GET: Profiles/Edit/5
+        public async Task<ActionResult> Edit(string email)
+        {
+            Profile myProfile = new Profile();
+            IAsyncSession session = db._driver.AsyncSession();
+            try
+            {
+                await session.ReadTransactionAsync(async tx =>
+                {
+                    var cursor =
+                        await tx.RunAsync(
+                            "match (p:Profile {Email: '" + User.Identity.Name + "'}) return p");
+
+                    IRecord Record = await cursor.SingleAsync();
+                    INode nodeProfile = (INode)Record.Values["p"];
+
+                    myProfile.HouseNumber = nodeProfile.Properties.Values.First().As<string>();
+                    myProfile.Email = nodeProfile.Properties.Values.Skip(1).First().As<string>();
+                    myProfile.Rate = nodeProfile.Properties.Values.Skip(2).First().As<int>();
+                    myProfile.FirstName = nodeProfile.Properties.Values.Skip(3).First().As<string>();
+                    myProfile.Street = nodeProfile.Properties.Values.Skip(4).First().As<string>();
+                    myProfile.PhoneNumber = nodeProfile.Properties.Values.Skip(5).First().As<string>();
+                    myProfile.City = nodeProfile.Properties.Values.Skip(6).First().As<string>();
+                    myProfile.Login = nodeProfile.Properties.Values.Skip(7).First().As<string>();
+                    myProfile.LastName = nodeProfile.Properties.Values.Skip(8).First().As<string>();
                 });
 
             }
@@ -127,27 +243,11 @@ namespace PROJEKT_PZ_NK_v3.Controllers
                 await session.CloseAsync();
             }
 
-            if (profile == null)
+            if (myProfile == null)
             {
                 return HttpNotFound();
             }
-            return View("Details", profile);
-        }
-
-        // GET: Profiles/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            var profile = db.Profiles.Find(id);
-
-            if (profile == null)
-            {
-                return HttpNotFound();
-            }
-            return View(profile);
+            return View(myProfile);
         }
 
         // POST: Profiles/Edit/5
@@ -159,12 +259,7 @@ namespace PROJEKT_PZ_NK_v3.Controllers
         {
             if (ModelState.IsValid)
             {
-                profile.Rate = 0;
-                db.Entry(profile).State = EntityState.Modified;
-                db.SaveChanges();
-
                 IAsyncSession session = db._driver.AsyncSession();
-
                 try
                 {
                     IResultCursor cursor = await session.RunAsync("match (a:Profile {Email: '" + User.Identity.Name + "'})" +
@@ -181,7 +276,6 @@ namespace PROJEKT_PZ_NK_v3.Controllers
                 {
                     await session.CloseAsync();
                 }
-                await db._driver.CloseAsync();
                 return RedirectToAction("Details");
             }
             return View(profile);
